8b58eea94a0c34d641552cf522f01d6ad655cc17
Gabriel Rodrigues <gabriel.estmr@gmail.com>
2024-08-27 13:57:06 -0300

    add

--- src/app_datomic_basic/core.clj
+++ src/app_datomic_basic/core.clj
@@ -1,7 +1,24 @@
 (ns app-datomic-basic.core
   (:require
-            [app-datomic-basic.configs.datomic :as db])
+            [app-datomic-basic.configs.datomic :as db]
+
+            [clojure-general-ms.gateways.ws.routes.app-routes :as app-routes]
+            [clojure-general-ms.gateways.ws.middlewares.custom-exception-handler :as custom-exception-handler]
+            [compojure.core :refer :all]
+            [clojure-general-ms.configs.mongo.mongo-config-doc :as mongo-config]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [ring.middleware.json :refer [wrap-json-response]]
+
+            )
   (:gen-class))
+
+
+(def app
+  (wrap-json-response (
+                        custom-exception-handler/custom-exception-handler
+                       app-routes/app-routes)))
+
+
 
 (defn -main []
   (let [
---
+++ src/app_datomic_basic/usecases/create_product.clj
@@ -0,0 +1,11 @@
+(ns app-datomic-basic.usecases.create-product
+  (:require [clojure-general-ms.domains.account :as account]
+            [clojure-general-ms.usecases.find-user-by-id :as find-user-by-id]))
+
+(defn execute [userDatabaseGateway accountDatabaseGateway]
+  (fn [account]
+    (let [find-user-by-id-fn (find-user-by-id/execute userDatabaseGateway)
+          user               (find-user-by-id-fn (account/get-user-id account))]
+      (when user
+        (let [saved-account (.save accountDatabaseGateway account)]
+          saved-account)))))
